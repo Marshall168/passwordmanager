fd685176a0408fd14da2e2c37af8379894287861
Scott Jones <60559239+Marshall168@users.noreply.github.com>
2022-08-26 17:45:12 +0100

    add load_key function

--- pass_manager.py
+++ pass_manager.py
@@ -1,13 +1,20 @@
 from cryptography.fernet import Fernet
 
+def load_key():
+    file = open('key.key', 'rb').read()
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key
 mast_pwd = input('Please enter the master password? ')
 
+'''
 def write_key():
     key = Fernet.generate_key()
     with open('key.key', 'wb') as key_file:
         key_file.write(key)
-
-write_key()
+'''
 
 def view():
     with open('passwords.txt', 'r') as f:
