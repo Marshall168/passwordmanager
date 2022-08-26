83df023fbe7504d1512ba4e30a2b00344a4e1e01
Scott Jones <60559239+Marshall168@users.noreply.github.com>
2022-08-26 17:38:47 +0100

    import cryptography

--- pass_manager.py
+++ pass_manager.py
@@ -1,20 +1,21 @@
+from cryptography.fernet import Fernet
 
 mast_pwd = input('Please enter the master password? ')
+
+
 
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split('|')
-
+            print('User: ', user,'| Password: ', passw)
 def add():
     name = input('Account Name: ')
     pwd = input('Password: ')
 
     with open('passwords.txt', 'a') as f:
         f.write(name + "|" + pwd + "\n")
-
-
 
 while True:
     mode = input('Would you like to add a password or view your passwords? (view, add)?\n or press "q" to quit ')
